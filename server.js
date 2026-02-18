d4594b2bb61dff239c98fad42028321ff7a14864
Yogesh Tiwari <119746250+yogeshtiwari12@users.noreply.github.com>
2026-02-18 21:38:39 +0530

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    // origin: "http://localhost:3000",
+    origin: "https://git-game-fmf6.vercel.app",
 }));
 
 app.get("/contributions/:username/:year", async (req, res) => {
